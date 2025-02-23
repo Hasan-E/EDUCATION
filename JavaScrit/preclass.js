85bf5fb4ef220c30a7ed26a51d351d0a56ef56ed
Hasan-E <hasan.erhan1302@gmail.com>
2025-02-24 00:03:03 +0300

    java script preclass1

---
+++ JavaScrit/preclass.js
@@ -0,0 +1,64 @@
+// ****************** Merhaba Dünya Örnekleri******************
+
+// Dokumana yazdırma
+
+// document.write("Merhaba Dünya");
+
+// Uyarı mesajı ile yazdırma
+
+// alert("Merhaba Dünya");
+
+// console.log ile yazdırma
+
+// console.log("Merhaba Dünya");
+// ******************DEĞİŞKEN TANIMLAMA ****************
+
+// var ile değişken tanımlama
+
+// var serverName = "ROOSTER";
+// console.log(serverName);
+
+// let ile değişkeni boş tanımlama
+
+let serverName;
+
+// let ile değişkene bilgi atamak
+serverName = "ROOSTER";
+console.log(serverName);
+// let ile değişkene bilgi atayarak tanımlamak
+
+let password = 1234;
+console.log(password);
+
+// değişken ataması yapmadan önce kullanmaya çalışmak
+/*  HATALI KULLANIM
+console.log(fullName);
+let fullName = "Rooster Big";
+*/
+
+let fullName = "Rooster Big";
+console.log(fullName);
+
+// let ile tanımlanan değişkenin içindeki bilgiyi değiştirmek
+
+fullName = "Rooster veryBig";
+console.log(fullName);
+
+// birleştirme veya ekleme işlemi
+
+fullName += " Rooster New";
+console.log(fullName);
+
+// const ile değişkeni boş tanımlamaya çalışmak
+
+//const serverPassword;//içi boş değişken tanımlandı
+
+// const ile değişken tanımlamak
+
+const SERVER_PASSWORD = "ROOOOOOSTR";
+console.log(SERVER_PASSWORD);
+
+/*
+SERVER_PASSWORD = "1324";
+console.log(SERVER_PASSWORD);
+*/
