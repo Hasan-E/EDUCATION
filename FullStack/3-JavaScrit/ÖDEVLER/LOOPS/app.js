93d7ef21ca461192e48e93a4998233be7fbeec54
Hasan-E <hasan.erhan1302@gmail.com>
2025-03-05 17:51:57 +0300

    furniture

--- FullStack/3-JavaScrit/ÖDEVLER/LOOPS/app.js
+++ FullStack/3-JavaScrit/ÖDEVLER/LOOPS/app.js
@@ -1,1 +1,17 @@
+let finish = "";
+let i = 1;
+let toplam = 0;
+do {
+  let not = prompt(i + ". notunuzu giriniz veya bitirmek için Q ya bas");
+  console.log(i + ".notunuz : " + not);
 
+  if (not == "q") {
+    break;
+  }
+  //   finish = prompt("bitirmek için Q ya bas").toUpperCase();
+  toplam += Number(not);
+  i++;
+} while (finish !== "Q");
+console.log(toplam);
+
+console.log((toplam / (i - 1)).toFixed(2));
