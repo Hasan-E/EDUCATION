18589d469632b02adfee0ff295ad1bec1255489d
Hasan-E <hasan.erhan1302@gmail.com>
2025-03-12 21:04:40 +0300

    js code challenge

--- FullStack/3-JavaScrit/Code-Challenges/challenges.js
+++ FullStack/3-JavaScrit/Code-Challenges/challenges.js
@@ -144,3 +144,75 @@
 // } else {
 //   console.log("Girilen sayı asal değildir");
 // }
+
+// !Sayının Tek mi Çift mi Olduğunu Bulma: Kullanıcıdan alınan sayının tek mi çift mi olduğunu kontrol eden bir fonksiyon yazın.
+
+// const sayi = +prompt("Lütfen bir sayı giriniz");
+
+// const tekCift = (sayi) =>
+//   sayi % 2 === 0 ? console.log("çift sayı") : console.log("tek sayi");
+
+// tekCift(sayi);
+
+//! Sayı 5'in Katı mı? Kullanıcının girdiği bir sayının 5'in katı olup olmadığını kontrol eden bir fonksiyon yazın.
+// const sayi = +prompt("Lütfen bir sayı giriniz");
+
+// const beskati = (sayi) =>
+//   sayi % 5 === 0
+//     ? console.log("sayı 5 e bölünür")
+//     : console.log("sayı 5 e bölünmez");
+
+// beskati(sayi);
+
+//! Pozitif mi Negatif mi? Kullanıcının girdiği sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir fonksiyon yazın.
+
+// let sayi;
+
+// const pozitifNegatif = (sayi) => {
+//   sayi = +prompt("Lütfen bir sayı giriniz");
+
+//   if (sayi === 0) {
+//     console.log("Girilen sayı 0 dır");
+//   } else if (sayi < 0) {
+//     console.log("Girilen sayı negatiftir");
+//   } else {
+//     console.log("Girilen sayı pozitifdir");
+//   }
+// };
+
+// pozitifNegatif();
+
+//! Sayıyı Mutlak Değere Çevirme: Kullanıcının girdiği negatif bir sayıyı pozitif hale getiren bir fonksiyon yazın
+
+// const sayi = +prompt("Lütfen bir sayi giriniz");
+
+// const mutlak = (sayi) =>
+//   sayi < 0 ? console.log(sayi * -1) : console.log(sayi);
+
+// mutlak(sayi);
+
+//! Bir Sayının Karesini Bulma: Kullanıcının girdiği sayının karesini hesaplayan bir fonksiyon yazın.
+
+// const sayi = +prompt("karesi alınacak sayıyı giriniz");
+
+// const kare = () => (true ? console.log(sayi * sayi) : console.log("a"));
+
+// kare();
+
+//! Girilen Sayının Kaç Basamaklı Olduğunu Bulma: Kullanıcıdan alınan sayının kaç basamaklı olduğunu bulan bir fonksiyon yazın (örn. 234  3 basamak).
+
+let sayi = prompt("Lütfen bir sayı giriniz");
+
+const basamaksay = () => {
+  if (sayi < 0) {
+    sayi = sayi * -1;
+  }
+  let basamak = 0;
+  while (sayi >= 1) {
+    sayi = sayi / 10;
+    basamak++;
+  }
+  return basamak;
+};
+
+console.log(`${sayi} sayısı ${basamaksay(sayi)} basamaklı bir sayıdır`);
