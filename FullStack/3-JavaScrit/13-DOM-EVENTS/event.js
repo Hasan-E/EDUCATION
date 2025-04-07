a441284734fdee140de23813652f20a0494a5c95
Hasan-E <hasan.erhan1302@gmail.com>
2025-04-07 22:11:19 +0300

    event2

---
+++ FullStack/3-JavaScrit/13-DOM-EVENTS/event.js
@@ -0,0 +1,51 @@
+//* ==============================================
+//*                  EVENTS
+//* ===============================================
+
+console.log("******Events******");
+
+//? Selectorler
+const h2 = document.querySelector("h2");
+const body = document.querySelector("body");
+const myInput = document.getElementById("input");
+const ul = document.querySelector("ul");
+const addBtn = document.getElementById("btn");
+
+// Event Tanımlama
+
+//Mouse h2 elementi üzerine geldiğinde tetiklenir
+h2.onmouseover = function () {
+  // console.log("tetiklendi")
+  // h2.style.color="red"
+  h2.classList.add("red", "center");
+};
+
+//Mouse h2 elementi üxerinden ayrıldığında tetiklenir
+h2.onmouseout = () => {
+  h2.classList.remove("red", "center");
+};
+
+//* DOM içeriklerinin tamamı yüklendikten sonra tetiklenen event (onload)
+body.onload = function () {
+  myInput.focus();
+};
+
+//? DOMContentLoaded (metin v.b içerikler yüklenince tetiklenir. Resim, link gibi içeriklerin yüklenmesini beklemez)
+// document.addEventListener("DOMContentLoaded",(e)=>{
+//     console.log("çalıştı")
+// })
+
+addBtn.addEventListener("click", (e) => {
+  // console.log(e.target)
+  // console.log(e.target.value)
+  // console.log(myInput.value)
+
+  if (!myInput.value) {
+    alert("lütfen boş bırakmayınız");
+  } else {
+    const li = document.createElement("li");
+    li.textContent = myInput.value;
+    ul.appendChild(li);
+    myInput.value = "";
+  }
+});
