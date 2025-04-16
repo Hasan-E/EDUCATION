034eb4e0de631f7813b6b772febde4956ef97a2f
Hasan-E <hasan.erhan1302@gmail.com>
2025-04-16 23:36:43 +0300

    flag

--- FullStack/3-JavaScrit/16-ASYNC/2 project/app.js
+++ FullStack/3-JavaScrit/16-ASYNC/2 project/app.js
@@ -11,6 +11,7 @@ fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
 
 function showScreen(data) {
   const mealsDiv = document.querySelector(".food");
+  mealsDiv.innerHTML = ""; // bu fonksiyon çok sefer çalışacak ve ekrana farklı elemanlar baslılacak, dolayısıyla her seferinde ekran temizlenmeli ve yeni veri basılmalı
   data.forEach((a) => {
     mealsDiv.innerHTML += `
     
@@ -24,10 +25,25 @@ function showScreen(data) {
 }
 
 //!  INPUTA GİRİLEN MEALS I EKRANA BASTIRMA
+//!arama inputuna yazdığım harfleri içeren yemekleri ekrana bastır (oninput=kullanıcı input elemanına her veri girişinde çalışmaktadır)
+
+//? e.target.value=> inputa her veri girişindeki değerleri alabilmek için kullanılır.document.querySelector("input").value=e.target.value
 
 document.querySelector("input").oninput = (e) => {
   //   console.log(e.target.value);
   let yemek = dizi.filter((b) =>
     b.strMeal.toLowerCase().includes(e.target.value.toLowerCase())
   );
+  showScreen(yemek);
 };
+
+//!bayraklara tıklanınca o ülkenin yemeği gelsin
+//!api yi aldığımız sitedeki url nin endpoint i ülke vatandaşları şeklinde, örneğin Turkish. bizde bayraklara tıklanınca hazır img gelmişken ülke vatandaşı da gelmiş olsun diye id sine bunu yerleştirdik, ve fetch ile yazdığımız url nin sonuna bu id yi ekleyerek tıkladığımız ülkenin yemeklerinin gelmesini sağladık
+
+document.querySelectorAll("img").forEach((bayrak) => {
+  bayrak.onclick = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${bayrak.id}`)
+      .then((res) => res.json())
+      .then((veri) => showScreen(veri.meals));
+  };
+});
