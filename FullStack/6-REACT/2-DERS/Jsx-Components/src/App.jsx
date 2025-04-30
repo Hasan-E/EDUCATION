9ae961b96875ab653549b1933abf7e35f29c2c50
Hasan-E <hasan.erhan1302@gmail.com>
2025-04-30 22:09:46 +0300

    react components

--- FullStack/6-REACT/2-DERS/Jsx-Components/src/App.jsx
+++ FullStack/6-REACT/2-DERS/Jsx-Components/src/App.jsx
@@ -1,28 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import roosterLogo from "./assets/r5.jpg";
-import "./App.css";
-
+import React from "react";
+import Msg from "./components/Msg";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Merhaba React</h1>
-      <img src={roosterLogo} className="logo rooster" alt="rooster logo" />
-      <p>Reacte Başlıyoruz</p>
-      <hr />
-      <h3>Yeni Kodlar Geliyor</h3>
-    </>
+    // jsx alanı
+    <div>
+      APP
+      <Msg />
+    </div>
   );
 }
 
