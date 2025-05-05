206a5e4c62458b7ce199794e05d645de05e500d6
Hasan-E <hasan.erhan1302@gmail.com>
2025-05-06 02:00:45 +0300

    teamwork 11

---
+++ FullStack/Team Work/M02/TW-11/dummyDataPage/src/helper/data.js
@@ -0,0 +1,32 @@
+const users = [
+  {
+    id: 1,
+    name: "Alex Johnson",
+    role: "Web Developer",
+    imageUrl: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    id: 2,
+    name: "Sam Wilson",
+    role: "UI Designer",
+    imageUrl: "https://randomuser.me/api/portraits/men/2.jpg",
+  },
+  {
+    id: 3,
+    name: "Taylor Smith",
+    role: "Backend Engineer",
+    imageUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+  },
+  {
+    id: 4,
+    name: "Jordan Lee",
+    role: "Fullstack Developer",
+    imageUrl: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    id: 5,
+    name: "ROOSTER",
+    role: "Fullstack Developer",
+    imageUrl: "../assets/r5.jpg",
+  },
+];
