2288526d2651e49f991d3705e155e9ca564cce7e
Hasan-E <hasan.erhan1302@gmail.com>
2025-05-06 20:50:20 +0300

    aaa

--- FullStack/Team Work/M02/TW-11/dummyDataPage/src/App.jsx
+++ FullStack/Team Work/M02/TW-11/dummyDataPage/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "./App.scss";
 import Header from "./components/header/Header";
 import Users from "./components/users/Users";
-import { users } from "./helper/data";
+import users from "./helper/data";
 // console.log({ users });
 
 function App() {
   return (
     <>
       <Header />
-    <Users users={users}/>
+      <Users users={users} />
     </>
   );
 }
