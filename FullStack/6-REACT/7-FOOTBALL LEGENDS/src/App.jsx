3ba3fc9a9bdb089708f30f1fe6c9d715ae432c6b
Hasan-E <hasan.erhan1302@gmail.com>
2025-05-13 01:09:46 +0300

    Hospital app start

--- FullStack/6-REACT/7-FOOTBALL LEGENDS/src/App.jsx
+++ FullStack/6-REACT/7-FOOTBALL LEGENDS/src/App.jsx
@@ -1,13 +1,14 @@
 // import Container from "react-bootstrap/Container"
-import { Container,Image } from "react-bootstrap";
-import "./App.css";
+import { Container, Image } from "react-bootstrap";
 import Header from "./components/header/Header";
+import LegendsContainer from "./components/legends/LegendsContainer";
 
 function App() {
   return (
     <>
       <Container className="text-center mt-5">
         <Header />
+        <LegendsContainer />
       </Container>
     </>
   );
---
+++ FullStack/6-REACT/7-FOOTBALL LEGENDS/src/components/legends/LegendsContainer.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import { data } from "../../helpers/data";
+import LegendCard from "./LegendCard";
+
+const LegendsContainer = () => {
+  return (
+    <Container className="my-3 p-3 card-container">
+      <Row xs={1} md={2} xl={3} className="justify-content-center g-3">
+        {data.map((legend) => (
+          <LegendCard legend={legend} key={legend.id} />
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default LegendsContainer;
