1e93e655f58a9f35f709a8aca0fa3b0c40388214
Hasan-E <hasan.erhan1302@gmail.com>
2025-05-16 21:10:22 +0300

    9.class 1.lesson

--- FullStack/6-REACT/9-LIFECYCLE-USEEFFECT/src/App.jsx
+++ FullStack/6-REACT/9-LIFECYCLE-USEEFFECT/src/App.jsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import roosterLogo from "./assets/r5.jpg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LifeCycleMethods from "./LifeCycleMethods";
+import { useState } from "react";
+import Clock from "./Clock";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [show, setShow] = useState(true);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Merhaba React</h1>
-      <img src={roosterLogo} className="logo rooster" alt="rooster logo" />
-      <p>Reacte Başlıyoruz</p>
-      <hr />
-      <h3>Yeni Kodlar Geliyor</h3>
-    </>
+    <div className="container text-center mt-4">
+      <button onClick={() => setShow(!show)} className="btn btn-danger">
+        SHOW
+      </button>
+      {/* {show && <LifeCycleMethods />} */}
+      {show && <Clock />}
+    </div>
   );
 }
 
