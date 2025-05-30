a2d75bedd31303530179027471881a1b613f741b
Hasan-E <hasan.erhan1302@gmail.com>
2025-05-30 21:17:54 +0300

    react tailwind basic

---
+++ FullStack/7-TAIL_WIND/1-Tailwind_css/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Typography from "./components/Typography";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center text-green-700 font-bold underline">
+        Hello Tailwind!
+      </h1>
+      ;
+      <Typography />
+    </>
+  );
+}
+
+export default App;
---
+++ FullStack/7-TAIL_WIND/1-Tailwind_css/src/components/Typography.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Typography = () => {
+  return (
+    // <div>Typography</div>
+    <>
+      <p className="text-2xl">Typography</p>
+      <p className="text-sm">Typography</p>
+      <p className="text-base/8">Typography</p>
+      <p className="text-lg">Typography</p>
+      <p className="text-xl">Typography</p>
+      <p className="text-2xl">Typography</p>
+      <h1 className="text-4xl">Başlık</h1>
+      <h2 className="text-3xl">Başlık</h2>
+      <h2 className="text-[60px]">Başlık</h2>
+    </>
+  );
+};
+
+export default Typography;
