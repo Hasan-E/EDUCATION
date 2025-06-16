0d4a0b33cb2fd97c2638d423ecfb46e8e7fb6b39
Hasan-E <hasan.erhan1302@gmail.com>
2025-06-16 21:15:25 +0300

    lesson 1

--- FullStack/6-REACT/15-MUI/src/components/MuiTypography.jsx
+++ FullStack/6-REACT/15-MUI/src/components/MuiTypography.jsx
@@ -1,15 +1,32 @@
-import { Container, Typography } from "@mui/material";
+import { Typography, Container,Box } from "@mui/material";
 
-const MuiTypography = ()=>{
-return( 
+const MuiTypography = () => {
+  return (
     <Container maxWidth="md">
-<Typography variant="h4" component="h2" gutterBottom color="primary.dark" fontWeight="bold">
-    Typography'nin Kütüphane Projesindeki önemi
-</Typography>
-<Typography variant="body1">
-    Typography'nin Kütüphane Projesindeki önemi
-</Typography>
+         <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Typography variant="h4" component="h2" gutterBottom color="primary.dark" fontWeight="bold">
+       Typography'nin Kütüphane Projesindeki Önemi
+      </Typography>
+      <Typography variant="body1" >
+          Kütüphane uygulamalarında yazı tipleri, kullanıcı deneyimini doğrudan etkiler. Okunabilirlik, tutarlılık ve doğru hiyerarşi ile kullanıcılar aradıkları bilgilere kolayca ulaşabilir.
+      </Typography>
+       <Typography variant="h6" gutterBottom fontWeight="medium" color="warning">
+          Okunabilirlik Öncelikli Olmalıdır
+        </Typography>
+        <Typography variant="body2" paragraph>
+          Uzun metinlerin rahat okunması için sade ve net fontlar tercih edilmeli, aşırı süslü fontlardan kaçınılmalıdır.
+        </Typography>
+    </Box>
+   
+
+        <Typography variant="h6" gutterBottom fontWeight="medium" color="success">
+          Hiyerarşi ve Vurgu
+        </Typography>
+        <Typography variant="body2" paragraph>
+          Başlıklar ile içerik metinleri arasında net farklar olmalı, başlıklar daha büyük ve kalın, metinler ise sade olmalıdır.
+        </Typography>
     </Container>
-)
-}
+  );
+};
+
 export default MuiTypography;
