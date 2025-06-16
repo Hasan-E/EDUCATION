a20a6a2faf7b435ef311fbeac917e859efea17c4
Hasan-E <hasan.erhan1302@gmail.com>
2025-06-16 21:03:59 +0300

    mui start

---
+++ FullStack/6-REACT/15-MUI/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MuiTypography from './components/MuiTypography'
+
+function App() {
+
+  return (
+    <>
+      <MuiTypography/>
+    </>
+  )
+}
+
+export default App
---
+++ FullStack/6-REACT/15-MUI/src/components/MuiTypography.jsx
@@ -0,0 +1,15 @@
+import { Container, Typography } from "@mui/material";
+
+const MuiTypography = ()=>{
+return( 
+    <Container maxWidth="md">
+<Typography variant="h4" component="h2" gutterBottom color="primary.dark" fontWeight="bold">
+    Typography'nin Kütüphane Projesindeki önemi
+</Typography>
+<Typography variant="body1">
+    Typography'nin Kütüphane Projesindeki önemi
+</Typography>
+    </Container>
+)
+}
+export default MuiTypography;
