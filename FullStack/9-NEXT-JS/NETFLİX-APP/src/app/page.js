77befdf67700ca364d677e608166748631767929
Hasan-E <hasan.erhan1302@gmail.com>
2025-07-17 19:56:37 +0300

    NEXTJS STARTER

---
+++ FullStack/9-NEXT-JS/NETFLİX-APP/src/app/page.js
@@ -0,0 +1,14 @@
+
+//? app folder içindeki page.js "/" route olan home sayfasıdır
+
+
+
+export default function Home() {
+  return (
+    <div className="relative h-screen w-full bg-fixed bg-no-repeat bg-cover ">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        HOME
+      </div>
+    </div>
+  );
+}
