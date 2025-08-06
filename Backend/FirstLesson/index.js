9ca38a4d78b3daa562e22299040046a558f1c08f
Hasan-E <hasan.erhan1302@gmail.com>
2025-08-06 20:06:12 +0300

    first lesson

---
+++ Backend/FirstLesson/index.js
@@ -0,0 +1,17 @@
+/*==================NODE.JS================================*/
+
+// Offical: https://nodejs.org/
+// Documents: https://nodejs.org/api
+
+/* ------------------ Node Shel --------------------------*/
+
+// $ node - nodejs ortamına terminalden ualşabilirim 
+// Herhangi bir js komutunu veya bir paketi indirebilirim 
+// .exit - nodejs runtime ortamından çıkmak için kullanılır 
+
+
+/* ------------------Index.js ------------------------ */
+// to run index.js file : node index.js
+
+const message = "hi CH19"
+console.log(message);
