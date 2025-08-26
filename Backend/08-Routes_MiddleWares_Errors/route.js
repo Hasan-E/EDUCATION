71db7e8eaca81009a4cf59ac8796e0d3530adaf6
Hasan-E <hasan.erhan1302@gmail.com>
2025-08-26 16:28:33 +0300

    router update

---
+++ Backend/08-Routes_MiddleWares_Errors/route.js
@@ -0,0 +1,33 @@
+"use scrict";
+
+/* ============================================ */
+/*                    ROUTES                    */
+/* ============================================ */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT ?? 8000;
+/* ============================================ *
+// app.route("/").get((req, res) => response.send({ method: "GET" }));
+
+//* "Router" is special app for URL control in Express
+const router = require("express").Router();
+
+
+
+app.use(router);
+/* ============================================ */
+//* Import router
+
+// const router = require("./routes/index.js");
+// const router = require("./routes/index");
+// const router = require("./routes");
+// app.use(router);
+
+app.use(require("./routes"));
+
+/* ============================================ */
+app.listen(PORT, () => console.log("Running at : http://127.0.0.1:" + PORT));
