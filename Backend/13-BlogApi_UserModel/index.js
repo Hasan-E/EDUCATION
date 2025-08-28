10230804155599a800b0fa7c29563b4f0836ed63
Hasan-E <hasan.erhan1302@gmail.com>
2025-08-28 19:37:41 +0300

    db

--- Backend/13-BlogApi_UserModel/index.js
+++ Backend/13-BlogApi_UserModel/index.js
@@ -3,12 +3,12 @@
      EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-require('dotenv').config();
-const PORT = process.env.PORT || 8000
-const HOST = process.env.HOST || 'localhost'
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || "localhost";
 
 /* ------------------------------------------------------- */
 // Middlewares & Configs
@@ -19,19 +19,19 @@ app.use(express.json());
 // DB Connection
 // const dbConnection = require('./src/dbConnection');
 // dbConnection()
-require('./src/dbConnection')();
+require("./src/dbConnection")();
 
 /* ------------------------------------------------------- */
 // Routes
-app.all('/', (req, res) => res.send('Welcome to Blog Api'));
+app.all("/", (req, res) => res.send("Welcome to Blog Api"));
 
 // Blog route
-app.use('/blogs', require('./src/routes/blogRouter'));
-app.use('/users', require('./src/routes/userRouter'));
+app.use("/blogs", require("./src/routes/blogRouter"));
+app.use("/users", require("./src/routes/userRouter"));
 
 /* ------------------------------------------------------- */
 // ErrorHandler
-app.use(require('./src/middlewares/errorHandler'));
+app.use(require("./src/middlewares/errorHandler"));
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log(`Running at: http://${HOST}:${PORT}`));
--- Backend/13-BlogApi_UserModel/src/dbConnection.js
+++ Backend/13-BlogApi_UserModel/src/dbConnection.js
@@ -3,12 +3,13 @@
      EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dbConnection = () => {
-    mongoose.connect(process.env?.DB_URI || 'mongodb://127.0.0.1:27017/blogAPI') // default db name 'test'
-        .then(() => console.log('* DB Connected *'))
-        .catch((err) => console.log('! DB Not Connected !', err))
-}
+  mongoose
+    .connect(process.env?.DB_URI || "mongodb://127.0.0.1:27017/blogAPI") // default db name 'test'
+    .then(() => console.log("* DB Connected *"))
+    .catch((err) => console.log("! DB Not Connected !", err));
+};
 
-module.exports = dbConnection
+module.exports = dbConnection;
