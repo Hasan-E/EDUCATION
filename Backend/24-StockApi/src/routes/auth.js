447ceeda76feafff8fa067a64ac4947ae8710e48
Hasan-E <hasan.erhan1302@gmail.com>
2025-09-22 23:20:09 +0300

    update auth controller and auth routes

--- Backend/24-StockApi/src/routes/auth.js
+++ Backend/24-StockApi/src/routes/auth.js
@@ -1,6 +1,16 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
+| FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
+const { login, refresh, logout } = require("../controllers/auth");
+
 /* ------------------------------------------------------- */
+
+// URL -> /auth
+
+router.post("/login", login);
+router.post("/refresh", refresh);
+router.post("/logout", logout);
+
+module.exports = router;
