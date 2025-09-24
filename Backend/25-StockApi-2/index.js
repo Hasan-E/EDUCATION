e467218a940b56be6bda04141fc160dde7a21c63
Hasan-E <hasan.erhan1302@gmail.com>
2025-09-24 20:05:36 +0300

    aa

--- Backend/25-StockApi-2/index.js
+++ Backend/25-StockApi-2/index.js
@@ -1,24 +1,24 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const express = require('express');
+const express = require("express");
 const app = express();
 
 /* ------------------------------------------------------- */
 // Required Modules:
 
 // envVariables to process.env:
-require('dotenv').config();
-const HOST = process.env?.HOST || '127.0.0.1'
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const HOST = process.env?.HOST || "127.0.0.1";
+const PORT = process.env?.PORT || 8000;
 
 app.set("query parser", "extended");
 /* ------------------------------------------------------- */
 // Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection');
+const { dbConnection } = require("./src/configs/dbConnection");
 dbConnection();
 
 /* ------------------------------------------------------- */
@@ -28,44 +28,44 @@ dbConnection();
 app.use(express.json());
 
 // Check Authentication:
-app.use(require('./src/middlewares/authentication'));
+app.use(require("./src/middlewares/authentication"));
 
 // Run Logger:
-app.use(require('./src/middlewares/logger'));
+app.use(require("./src/middlewares/logger"));
 
 // Query Handler:
-app.use(require('./src/middlewares/queryHandler'));
+app.use(require("./src/middlewares/queryHandler"));
 
 /* ------------------------------------------------------- */
 // Routes:
 
 // HomePath:
-app.all('/', (req, res) => {
-    res.send({
-        error: false,
-        message: 'Welcome to Stock Management API',
-        documents: {
-            swagger: '/documents/swagger',
-            redoc: '/documents/redoc',
-            json: '/documents/json',
-        },
-        user: req.user
-    })
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to Stock Management API",
+    documents: {
+      swagger: "/documents/swagger",
+      redoc: "/documents/redoc",
+      json: "/documents/json",
+    },
+    user: req.user,
+  });
 });
 
 // Static Route:
-app.use('/upload', express.static('./upload'));
+app.use("/upload", express.static("./upload"));
 
 // Routes:
-app.use(require('./src/routes'));
+app.use(require("./src/routes"));
 
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
